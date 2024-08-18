a37d84d288e0c70d65e2ac8cf42942b54b52f80e
Andrew Morgan <58256684+AndrewMorgan2@users.noreply.github.com>
2024-08-18 21:34:36 +0100

    start

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo_main} width="20%" alt="Main Logo"/>
+      <header style={{
+        backgroundColor: 'black',
+        height: '30px', // Adjust this value to make the bar smaller or larger
+        width: '100%'
+      }}>
+
       </header>
+      <main>
+        <div class="mainClass">
+          <img src={logo_main} width="20%" alt="Main Logo"/>
+          <div class="textMainClass">
+            <h2 >We sell wood or something</h2>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
